2c0094524da67a27ae4be353d58245bc36390620
Luis Diego Araya Campos <luisdiegoarayacampos@hotmail.com>
2023-04-29 16:35:58 -0600

    Agrego documentacion
    Signed-off-by: Luis Diego Araya Campos <luisdiegoarayacampos@hotmail.com>

--- Excepciones/MyException.cpp
+++ Excepciones/MyException.cpp
@@ -1,3 +1,8 @@
+/*! \file MyException.cpp
+  Declaración del método what() donde se nota que devulve la cadena de
+  caracteres "My custom exception".
+*/
+
 #include "MyException.hpp"
 
 const char* MyException::what() const throw() {
--- Excepciones/MyException.hpp
+++ Excepciones/MyException.hpp
@@ -1,10 +1,27 @@
+/*! \file MyException.hpp
+  Header para la clase MyException.
+*/
+
 #ifndef var_cont_func_my_exception
 #define var_cont_func_my_exception
 
 #include <iostream>
 
+/*!
+  Clase MyException que hereda de manera pública de la clase std::exception
+*/
+
 class MyException : public std::exception {
 public:
+  /*!
+    El método what() es también un método de la clase std::exception,
+    entonces se reescribe para devolver un mensaje personalizado. Este método
+    devuelve un puntero constante a la primera posición de una cadena de
+    caracteres. Este método también presenta la palabra clave const luego del
+    nombre del método lo que quiere decir que no tiene permitido modificar el
+    objeto que lo invoca. Finalmente aparece throw() que previene al método
+    what() de lanzar una excepción. 
+  */
   const char* what() const throw();
 };
 
